082c144299a03eda4a2d1f2b2b67a15d43623aea
Jiří Vrba <jiri@vrba.dev>
2020-11-10 20:50:02 +0100

    WIP show domains

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -1,6 +1,25 @@
 class DomainsController < ApplicationController
   def index
     @domains = Domain.all
-    @indexed, @not_indexed = @domains.partition { |domain| domain.last_indexed_at.nil? }
+    @indexed, @not_indexed = @domains.partition(&:indexed?)
+  end
+
+  def new
+    # Just render the template, nothing special to be seen here...
+  end
+
+  def create
+    @domain = Domain.create create_params
+    redirect_to domains_path
+  end
+
+  def show
+    @domain = Domain.find params[:id]
+  end
+
+  private
+
+  def create_params
+    params.require(:domain).permit(:url)
   end
 end
--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,3 +1,7 @@
 class Domain < ApplicationRecord
   validates :url, presence: true, uniqueness: true
+
+  def indexed?
+    !last_indexed_at.nil?
+  end
 end
